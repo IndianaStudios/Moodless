837a9095cd36335f2cb4e80b5d7ea2637f6d0316
IndianaStudios <indianasainzpalacios@gmail.com>
2026-04-09 16:36:11 +0200

    Debug server errors

--- api/search-youtube.ts
+++ api/search-youtube.ts
@@ -43,6 +43,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     return res.status(200).json(data);
   } catch (error: any) {
     console.error('Search Proxy Error:', error.message);
-    return res.status(500).json({ error: 'Internal server error' });
+    return res.status(500).json({ error: `Internal Server Error: ${error.message}` });
   }
 }
